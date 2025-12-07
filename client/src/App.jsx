824eaa8d237655a7cebf15edf3024d38930db1db
SCV <scv_c@naver.com>
2025-12-08 00:29:38 +0900

    251208_toast줄바꿈수정
    <br>태그로 줄바꿈 유도

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
 
       showToast(
         "default",
-        `새로운 회차 최고득점 ${score}! 축하드립니다. \n ${userList.join("\n")}`
+        `새로운 회차 최고득점 ${score}!<br />축하드립니다.<br />${userList.join("<br />")}`
       );
     });
   }, []);
